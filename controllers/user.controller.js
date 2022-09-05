c49d861aa50033883df62abd7183215e6f84337a
Emivw <emiya.vanwyk@gmail.com>
2022-09-05 09:05:16 +0200

    login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -80,7 +80,7 @@ userRouter.post('/', async (req, res, next) => {
 userRouter.put('/:id', async (req, res, next) => {
     try {
         const userName = req.body.user.userName;
-        const role_id = req.body.user.role;
+        const role_id = req.body.user.role_id;
         const email = req.body.user.email;
         let password = req.body.user.password;
         const id = req.params.id;
